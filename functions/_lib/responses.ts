27e48deed4cc2b865321ee55ffc788ff6763c251
readthem <233414342+readthem@users.noreply.github.com>
2025-09-21 19:56:04 +1000

    Update responses.ts

--- functions/_lib/responses.ts
+++ functions/_lib/responses.ts
@@ -1,6 +1,11 @@
-export const json = (obj: any, init: ResponseInit = {}) =>
-  new Response(JSON.stringify(obj), { headers: { 'content-type': 'application/json' }, ...init });
+export const json = (data: unknown, init: ResponseInit = {}) =>
+  new Response(JSON.stringify(data), { status: 200, headers: { 'content-type': 'application/json', ...(init.headers||{}) } })
 
-export const badRequest = (msg = 'Bad Request') => new Response(msg, { status: 400 });
-export const unauthorized = (msg = 'Unauthorized') => new Response(msg, { status: 401 });
-export const notFound = (msg = 'Not Found') => new Response(msg, { status: 404 });
+export const badRequest = (msg = 'Bad Request') =>
+  new Response(msg, { status: 400 })
+
+export const unauthorized = (msg = 'Unauthorized') =>
+  new Response(msg, { status: 401 })
+
+export const notFound = (msg = 'Not found') =>
+  new Response(msg, { status: 404 })
